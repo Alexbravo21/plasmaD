8b26374822229b39a287e4cb46d81710b989f3e8
Alejandro Bravo <alexbravo21@gmail.com>
2020-08-30 18:39:49 -0500

    Se añadió el mensaje debajo del hero con el contenido de contentful

--- src/app/components/below-hero/below-hero.component.ts
+++ src/app/components/below-hero/below-hero.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ContentfulService } from '../../services/contentful.service';
+import { Entry } from 'contentful';
 
 @Component({
-  selector: 'app-below-hero',
-  templateUrl: './below-hero.component.html',
-  styleUrls: ['./below-hero.component.scss']
+    selector: 'app-below-hero',
+    templateUrl: './below-hero.component.html',
+    styleUrls: ['./below-hero.component.scss']
 })
 export class BelowHeroComponent implements OnInit {
+    belowHero: string;
 
-  constructor() { }
+    constructor(private contentful: ContentfulService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.contentful.getMultipleContent('belowHero', 1).subscribe((data => {
+            this.belowHero = data.items[0].fields.texto;
+        }))
+    }
 
 }
