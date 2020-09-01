a7684f6f6a1c9f13f13ce923bbe4344fa69a5a6e
Alejandro Bravo <alexbravo21@gmail.com>
2020-09-01 17:26:46 -0500

    Se añadió el carousel al hero con el contenido que llega de Contentful

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 import { ContentfulService } from '../../services/contentful.service';
+import bulmaCarousel from '../../../../node_modules/bulma-carousel/dist/js/bulma-carousel.min.js';
+import { Entry } from 'contentful';
 
 @Component({
     selector: 'app-hero',
@@ -11,20 +13,42 @@ export class HeroComponent implements OnInit {
     @Input() heroProyecto: string;
     @Input() parentDetector: string;
     heroImage: string;
+    heroImages: Entry<any>[] = [];
+    slides: number = 5;
+    showCarousel = false;
 
     constructor(private contentful: ContentfulService) { }
 
     ngOnInit(): void {
-        if(this.parentDetector === 'home'){
-            this.contentful.getMultipleContent('heroImages', 1).subscribe((data => {
+        if (this.parentDetector === 'home') {
+            this.contentful.getMultipleContent('heroImages', this.slides).subscribe((data => {
+                this.heroImages = data.items;
+                console.log(this.heroImages);
                 this.heroImage = data.items[0].fields.imagenBanner.fields.file.url;
+                setTimeout(() => {
+                    this.initiateCarousel();
+                }, 500)
             }))
         }
     }
-    ngOnChanges(changes: SimpleChanges){
-        if(changes.heroProyecto){
+    ngOnChanges(changes: SimpleChanges) {
+        if (changes.heroProyecto) {
             this.heroImage = this.heroProyecto;
         }
+    }
+
+    initiateCarousel() {
+        // Initialize all elements with carousel class.
+        bulmaCarousel.attach('.carousel', {
+            slidesToScroll: 1,
+            slidesToShow: 1,
+            pagination: false,
+            loop: true,
+            infinite: true
+        });
+        setTimeout(() => {
+            this.showCarousel = true;
+        }, 50)
     }
 
 
